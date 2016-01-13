9096ba0db546ea0a5705962f8ff360f3d59345f9
Alec C. Booth <alecbooth@ccs.k12.in.us>
2016-01-13 17:34:08 -0500

    tried slick

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.slideDiv').slick({
+		setting-name: setting-value
+	});
+});
+$('.autoplay').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 1000,
+});
+$('.single-item').slick();
